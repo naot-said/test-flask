9b30e4acef150522d1ab6b86c48b09d5c3491d02
naot-said <naotosaido@gmail.com>
2012-12-04 02:43:48 +0900

    commit

--- hello.py
+++ hello.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from flask import request
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -26,6 +28,13 @@ def projects():
 @app.route('/about')
 def about():
     return 'The about page'
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    if request.method == 'POST':
+        return 'do_the_login'
+    else:
+        return 'show_the_login_form'
 
 if __name__== '__main__':
     app.debug = True
